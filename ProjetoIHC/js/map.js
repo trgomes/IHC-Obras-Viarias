27e64cd9ad1c9b304fa354aab7c6d0d5ba8b25e5
tgomes <trtg10@gmail.com>
2016-05-18 08:52:43 -0300

    Update mapa

--- ProjetoIHC/js/map.js
+++ ProjetoIHC/js/map.js
@@ -1,71 +1,3 @@
-//var map;
-//
-//function initialize() {
-//    var latlng = new google.maps.LatLng(-23.2214924, -45.8998116);
-//
-//    var options = {
-//        zoom: 12,
-//        center: latlng,
-//        mapTypeId: google.maps.MapTypeId.ROADMAP
-//    };
-//
-//    map = new google.maps.Map(document.getElementById("mapa"), options);
-//
-//}
-//
-//function initMap() {
-//
-//    var icon1 = 'http://i.stack.imgur.com/rU427.png'; //verde
-//    var icon2 = 'https://maps.google.com/mapfiles/ms/icons/yellow-dot.png'; //amarelo
-//    var icon3 = 'http://i.stack.imgur.com/cdiAE.png'; //vermelho
-//
-//
-//    //string com formatação das informacoes, imagens e valores das obras
-//
-//    $.getJSON('https://api.myjson.com/bins/1ip3e', function (dados) {
-//        for (var i = 0; i < dados.length; i++) {
-//
-//            $.getJSON('https://api.myjson.com/bins/1ip3e', function (dados) {
-//                for (var i = 0; i < dados.length; i++) {
-//
-//                    var teste = '<div id="content" style="height: 390px; width: 498px;">' +
-//                            '<img src="' + dados[i].URL + '" width="498" height="280"/>' +
-//                            '<p style="font-size:14px; margin-top:10px;"> Valor: ' + dados[i].Valor + '</p>' +
-//                            '<p style="font-size:14px;">Descrição:' + dados[i].Descricao + '</p>' +
-//                            '</div>';
-//
-//                    $(document.body).append(teste);
-//
-//                }
-//            });
-//
-//            var infowindow = new google.maps.InfoWindow({
-//                content: contentString
-//            });
-//
-//            var myLatLng = {lat: $(document.body).append(dados[i].Latitude), lng: $(document.body).append(dados[i].Longitude)};
-//
-//            var marker = new google.maps.Marker({
-//                position: myLatLng,
-//                map: map,
-//                icon: $(document.body).append(dados[i].status),
-//                title: $(document.body).append(dados[i].Titulo)
-//            });
-//
-//            marker.addListener('click', function () {
-//                infowindow.open(map, marker);
-//            });
-//
-//
-//        }
-//    });
-//
-//}
-//
-//initialize();
-//initMap();
-
-
 // *
 // * Adicionar multiplos marcadores
 // * 2013 - www.marnoto.com
@@ -75,6 +7,37 @@
 var map;
 var infoWindow;
 
+// A variável markersData guarda a informação necessária a cada marcador
+// Para utilizar este código basta alterar a informação contida nesta variável
+//var markersData = [
+//   {
+//      lat: 40.6386333,
+//      lng: -8.745,
+//      nome: "Parque de Campismo Praia da Barra",
+//      morada1:"Rua Diogo Cão, 125",
+//      morada2: "Praia da Barra",
+//      codPostal: "3830-772 Gafanha da Nazaré" // não colocar virgula no último item de cada maracdor
+//   },
+//   {
+//      lat: 40.59955,
+//      lng: -8.7498167,
+//      nome: "Parque de Campismo da Costa Nova",
+//      morada1:"Quinta dos Patos, n.º 2",
+//      morada2: "Praia da Costa Nova",
+//      codPostal: "3830-453 Gafanha da Encarnação" // não colocar virgula no último item de cada maracdor
+//   },
+//   {
+//      lat: 40.6247167,
+//      lng: -8.7129167,
+//      nome: "Parque de Campismo da Gafanha da Nazaré",
+//      morada1:"Rua dos Balneários do Complexo Desportivo",
+//      morada2: "Gafanha da Nazaré",
+//      codPostal: "3830-225 Gafanha da Nazaré" // não colocar virgula no último item de cada maracdor
+//   } // não colocar vírgula no último marcador
+//];
+
+
+
 function initialize() {
     var mapOptions = {
         center: new google.maps.LatLng(-23.2214924, -45.8998116),
@@ -82,7 +45,7 @@ function initialize() {
         mapTypeId: 'roadmap',
     };
 
-    map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
     // cria a nova Info Window com referência à variável infowindow
     // o conteúdo da Info Window será atribuído mais tarde
@@ -101,18 +64,28 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 // Esta função vai percorrer a informação contida na variável markersData
 // e cria os marcadores através da função createMarker
-
-var icon1 = 'http://i.stack.imgur.com/rU427.png'; //verde
-var icon2 = 'https://maps.google.com/mapfiles/ms/icons/yellow-dot.png'; //amarelo
-var icon3 = 'http://i.stack.imgur.com/cdiAE.png'; //vermelho
-
-
 function displayMarkers() {
-
 
     // esta variável vai definir a área de mapa a abranger e o nível do zoom
     // de acordo com as posições dos marcadores
     var bounds = new google.maps.LatLngBounds();
+
+    // Loop que vai estruturar a informação contida em markersData
+    // para que a função createMarker possa criar os marcadores 
+//    for (var i = 0; i < markersData.length; i++) {
+//
+//        var latlng = new google.maps.LatLng(markersData[i].lat, markersData[i].lng);
+//        var nome = markersData[i].nome;
+//        var morada1 = markersData[i].morada1;
+//        var morada2 = markersData[i].morada2;
+//        var codPostal = markersData[i].codPostal;
+//
+//        createMarker(latlng, nome, morada1, morada2, codPostal);
+//
+//        // Os valores de latitude e longitude do marcador são adicionados à
+//        // variável bounds
+//        bounds.extend(latlng);
+//    }
 
     //Adicionado por Thiago em 11-05-2016
     $.getJSON('https://api.myjson.com/bins/1ip3e', function (dados) {
@@ -133,7 +106,7 @@ function displayMarkers() {
 //            $(document.body).append(teste);
 
             createMarker(latlng, url, valor, descricao, icone, titulo);
-
+            
             bounds.extend(latlng);
 
         }
@@ -152,7 +125,6 @@ function createMarker(latlng, url, valor, descricao, icone, titulo) {
     var marker = new google.maps.Marker({
         map: map,
         position: latlng,
-        icon: icone,
         title: titulo
     });
 
@@ -160,12 +132,18 @@ function createMarker(latlng, url, valor, descricao, icone, titulo) {
     // Define o conteúdo e abre a Info Window.
     google.maps.event.addListener(marker, 'click', function () {
 
+        // Variável que define a estrutura do HTML a inserir na Info Window.
+//        var iwContent = '<div id="iw_container">' +
+//                '<div class="iw_title">' + nome + '</div>' +
+//                '<div class="iw_content">' + morada1 + '<br />' +
+//                morada2 + '<br />' +
+//                codPostal + '</div></div>';
         var estruturaHtml = '<div id="content" style="height: 390px; width: 498px;">' +
                 '<img src="' + url + '" width="498" height="280"/>' +
                 '<p style="font-size:14px; margin-top:10px;"> Valor: ' + valor + '</p>' +
                 '<p style="font-size:14px;">Descrição:' + descricao + '</p>' +
                 '</div>';
-
+        
 
         // O conteúdo da variável iwContent é inserido na Info Window.
         infoWindow.setContent(estruturaHtml);
